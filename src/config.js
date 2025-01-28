38fec283277c138ee2f0cc8226e2cb4ef786f8bf
Nilay Türker <nilaytr89qgmail.com>
2025-01-28 20:08:12 +0300

    expect button done

---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const API_KEY = "48383034-506c7f8ffe99ad1c3f13eb63b"; 
+export const BASE_URL = "https://pixabay.com/api/?";
---
+++ src/main.js
@@ -0,0 +1,146 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import axios from "axios";
+
+import { API_KEY, BASE_URL } from "./config.js";
+
+const form = document.getElementById('search-form');
+const loadMoreBtn = document.getElementById('load-more');
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+let page = 1;
+let currentQuery = "";
+const perPage = 20;
+
+const searchParams = new URLSearchParams({
+    key: API_KEY,
+    q: "",
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: "true",
+    per_page: perPage,
+});
+
+async function searchImages() {
+    const query = form.querySelector("input[name='query']").value.trim();
+    
+    if (!query) {
+    iziToast.warning({
+      title: "Warning",
+      message: "Please enter a search query!",
+      position: "topRight",
+    });
+    return;
+    }
+    
+    if (query !== currentQuery) {
+    page = 1;
+    currentQuery = query;
+    gallery.innerHTML = "";
+    loadMoreBtn.style.display = "none";
+    }
+    
+    searchParams.set("q", currentQuery);
+    searchParams.set("page", page);
+
+    const url = `${BASE_URL}${searchParams.toString()}`;
+
+    try {
+    loader.classList.remove("hidden");
+
+    const response = await axios.get(url);
+    const data = response.data;
+
+        if (data.hits.length === 0) {
+        loadMoreBtn.style.display = 'none';
+        iziToast.error({
+        title: '',
+        message: 'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+    } else {
+      displayImages(data.hits);
+      loadMoreBtn.style.display = "block";
+      page++;
+            
+      const totalHits = data.totalHits;
+      const maxPages = Math.ceil(totalHits / perPage);
+      if (page > maxPages) {
+        loadMoreBtn.style.display = "none";
+        iziToast.info({
+          title: "",
+          message: "You have reached the end of the results.",
+          position: "topRight",
+        });
+      }
+    }
+  } catch (error) {
+    iziToast.error({
+      title: '',
+      message: `Sorry, ${error.message}! Please try again!`,
+      position: 'topRight',
+    });
+    console.error(error);
+  } finally {
+    loader.classList.add('hidden');
+  }
+}
+  
+function displayImages(images) {
+    const galleryHtml = images.map(({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads }) => {
+        return `<li class= "gallery-list">
+                <div class="gallery-div">
+                   <a class= "gallery-link" href="${largeImageURL}">
+                     <img src="${webformatURL}"  alt="${tags}" class="gallery-image"/>
+                   </a>
+                </div>
+                <ul class="card-info">
+                <li class="info-item">
+                   <h3>Likes</h3>
+                   <p>${likes}</p>
+                </li>
+                <li class="info-item">
+                  <h3>Views</h3>
+                  <p>${views}</p>
+                </li>
+                <li class="info-item">
+                  <h3>Comments</h3>
+                  <p>${comments}</p>
+                </li>
+                <li class="info-item">
+                   <h3>Downloads</h3>
+                   <p>${downloads}</p>
+                </li>
+                </ul>
+                </li>`;
+    }).join("");
+
+    gallery.innerHTML += galleryHtml;
+            
+    const lightbox = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+    lightbox.refresh();
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchImages();
+});
+
+loadMoreBtn.addEventListener("click", () => {
+  searchImages();
+});
